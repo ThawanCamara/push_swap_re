a78f948958f44f1a304a5bd8783e65f0e0d35630
Thawan Camara <thawancamara@gmail.com>
2025-06-18 13:17:05 -0300

    updating main code with input checks and stack prints

--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-souz <tde-souz@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 13:00:53 by tde-souz          #+#    #+#             */
+/*   Updated: 2025/06/17 21:05:18 by tde-souz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int main(void)
+int	main(int argc, char *argv[])
 {
-    printf("Hello there\n");
+	t_app	app;
+	
+	if (check_input(argc, argv))
+		return (1);
 
-    return (0);
+	app.stack_a = stack_init(argc, argv);
+	stack_print(app.stack_a);
+
+	// Clearing
+	stack_clear(app.stack_a);
+	stack_clear(app.stack_b);
+	return (0);
 }
