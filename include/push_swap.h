078626bffdbe80dd8ca56ecda91f6c6bbbbe44f1
Thawan Camara <thawancamara@gmail.com>
2025-06-18 13:14:40 -0300

    adding stack utility

--- include/push_swap.h
+++ include/push_swap.h
@@ -17,7 +17,20 @@
 # include <unistd.h>
 # include "list.h"
 
+typedef struct s_app
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+}	t_app;
+
+typedef struct s_data
+{
+	int	value;
+}	t_data;
 
 int		check_input(int argc, char *argv[]);
+t_list	*stack_init(int argc, char *argv[]);
+void	stack_print(t_list *stack);
+void	stack_clear(t_list *stack);
 
 #endif
